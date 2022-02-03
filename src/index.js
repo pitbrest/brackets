041ff59d000cdec698143ebd2a67fe0bf9b5b2cf
pitbrest <sasha.petrov@tut.by>
2022-02-03 10:28:39 +0300

    some commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	const openBrackets = ['(', '[', '{', '|']
+	const bracketsPair = {
+		')': '(',
+		']': '[',
+		'}': '{',
+		'|': '|',
+	}
+	let stack = []
+
+	for (let i = 0; i < check.arguments[0].length; i++) {
+
+		let currentSymbol = check.arguments[0][i]
+		console.log(currentSymbol);
+
+		if (openBrackets.includes(currentSymbol)) {
+			stack.push(currentSymbol)
+			console.log(stack);
+
+		} else {
+			if (stack.length === 0) {
+				return false
+			}
+
+			let lastElem = stack[stack.length - 1]
+
+			if (bracketsPair[currentSymbol] === lastElem) {
+				stack.pop()
+			} else {
+				return false
+			}
+		}
+	}
+	return stack.length === 0
 }
