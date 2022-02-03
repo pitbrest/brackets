f2742ded184bd24ea0b4962afaaa9591a9d59ab3
pitbrest <sasha.petrov@tut.by>
2022-02-03 10:53:59 +0300

    some commit

--- src/index.js
+++ src/index.js
@@ -11,11 +11,9 @@ module.exports = function check(str, bracketsConfig) {
 	for (let i = 0; i < check.arguments[0].length; i++) {
 
 		let currentSymbol = check.arguments[0][i]
-		console.log(currentSymbol);
-
+		
 		if (openBrackets.includes(currentSymbol)) {
-			stack.push(currentSymbol)
-			console.log(stack);
+			stack.push(currentSymbol)			
 
 		} else {
 			if (stack.length === 0) {
@@ -31,5 +29,7 @@ module.exports = function check(str, bracketsConfig) {
 			}
 		}
 	}
-	return stack.length === 0
+	if (stack.length === 0) {return true}
+	if (stack.length !== 0 && stack[0] === stack[1]) {return true}
+	else {return false}
 }
